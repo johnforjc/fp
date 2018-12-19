cfe43ffa8bca7decd5c17d0937805e8d2ea28f2f
johnforjc <44581271+johnforjc@users.noreply.github.com>
2018-12-19 11:32:40 +0700

    update posts

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -7,10 +7,26 @@ use TestCentreGym\Log;
 use TestCentreGym\User;
 use TestCentreGym\Soal;
 class LogController extends Controller
-{ 
+{
     public function index(){
         $id = auth()->user('id');
         $soal = Log::find($id);
         return view('log.log')->with('user',$soal);
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'id' => 'required',
+            'email' => 'required',
+            'nilai' => 'required'
+        ]);
+
+        $log = new Log;
+        $log->id=$request->input('id');
+        $log->email=$request->input('email');
+        $log->nilai=$request->input('nilai');
+        $log->save();
+
+        return redirect('/log');
     }
 }
--- database/migrations/2018_12_16_075241_create_posts_table.php
+++ database/migrations/2018_12_16_075241_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->mediumText('body');
             $table->timestamps();
         });
     }
